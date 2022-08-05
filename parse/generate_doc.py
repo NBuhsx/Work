5e468e6f8927995f2f5b465452aaf772a691d432
NBuhsx <trubitsyn98@gmail.com>
2022-08-06 02:36:49 +0300

    schema remark md

--- parse/generate_doc.py
+++ parse/generate_doc.py
@@ -6,7 +6,26 @@ from json_schema_for_humans.generate import generate_from_filename
 
 DIR = Path(Path(__file__).resolve().parent, 'schema')
 
-with open(Path(DIR, 'SchemaConfig.json'), mode='w') as file:
-    file.write(Config.schema_json(indent=2))
 
-generate_from_filename(Path(DIR, 'SchemaConfig.json'), Path(DIR, 'schema_doc.md'))
+
+
+def generate_html():
+    with open(Path(DIR, 'SchemaConfig.json'), mode='w') as file:
+        file.write(Config.schema_json(indent=2))
+    generate_from_filename(
+        schema_file_name=Path(DIR, 'SchemaConfig.json'), 
+        result_file_name=str(Path(DIR, 'schema_doc.html')))
+
+def generate_md():
+    with open(Path(DIR, 'SchemaConfig.json'), mode='w') as file:
+        file.write(Config.schema_json(indent=2))
+    generate_from_filename(
+        schema_file_name=Path(DIR, 'SchemaConfig.json'), 
+        result_file_name=str(Path(DIR, 'schema_doc.md')), 
+        copy_js=False,
+        copy_css=False)
+
+
+if __name__ == "__main__":
+    generate_html()
+    generate_md()
